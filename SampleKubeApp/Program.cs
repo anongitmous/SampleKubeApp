06158d15b76f01ac44bdb4571a823f72ce736336
anongitmous <registrations@ananswerback.com>
2021-11-17 14:29:52 -0700

    Add project files.

---
+++ SampleKubeApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SampleKubeApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpOperationResponse = await GetItems(args[0], args[1]);
+            var body = httpOperationResponse.Body;
+            foreach (var item in body.Items)
+            {
+                if (null != item?.Metadata?.Name)
+                {
+                    Console.WriteLine($"Pod name: {item.Metadata.Name}");
+                }
+                else
+                {
+                    Console.Write("no data");
+                }
+            }
+        }
+
+        static async Task<Microsoft.Rest.HttpOperationResponse<k8s.Models.V1PodList>> GetItems(string kubeCfg, string kubeCtx)
+        {
+            using var client = new k8s.Kubernetes(k8s.KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeCfg, kubeCtx));
+            return await client.ListPodForAllNamespacesWithHttpMessagesAsync(null, null, null, null, null, null,
+                null, null, null, null, null).ConfigureAwait(false);
+        }
+    }
+}
